039fbbb2a0a2294c1c5ae970e9f7ff403d550509
mohesem <mohesem@gmail.com>
2020-08-17 17:42:30 +0000

    i dont care

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const MBTiles = require("@mapbox/mbtiles");
 new MBTiles("/root/repos/fans_club/mbTiles/0.mbtiles", (err, mbtiles) => {
   mbtiles.getInfo(function (err, info) {
-    console.log(info); // info
+    console.log(info.tilestats); // info
   });
 });
