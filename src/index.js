416bd5173a20dd69af41fbf1de3deea205c0bbb0
mohesem <mohesem@gmail.com>
2020-08-17 17:40:26 +0000

    initial release

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const MBTiles = require("@mapbox/mbtiles");
 new MBTiles("/root/repos/fans_club/mbTiles/0.mbtiles", (err, mbtiles) => {
-  mbtilesgetInfo(function (err, info) {
+  mbtiles.getInfo(function (err, info) {
     console.log(info); // info
   });
 });
