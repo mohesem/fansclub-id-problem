bfe049a7ef983afd9c4ee5c7b5897c43e11d63f8
mohesem <mohesem@gmail.com>
2020-08-17 17:56:34 +0000

    i dont care

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ new MBTiles("/root/repos/fans_club/mbTiles/0.mbtiles", (err, mbtiles) => {
   mbtiles.getInfo(function (err, info) {
     console.log(info); // info
   });
-  //   mbtiles.getTile(0, 0, 0, (err, tile) => {
-  //     if (err) {
-  //       log(err);
-  //       return cb(404, headerErr, `Tile rendering error: ${err}\n`);
-  //     }
-  //     return cb(200, header, tile);
-  //   });
+  mbtiles.getTile(0, 0, 0, (err, tile) => {
+    if (err) {
+      console.log(err);
+      // return cb(404, headerErr, `Tile rendering error: ${err}\n`);
+    }
+    {
+      console.log(tile);
+    }
+    //   return cb(200, header, tile);
+  });
 });
