ce40602b3692d2a98ecb85d06e50e01717017627
mohesem <mohesem@gmail.com>
2020-08-17 18:45:04 +0000

    i dont care

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import geobuf from "geobuf";
 import execa from "execa";
 const MBTiles = require("@mapbox/mbtiles");
 
-new MBTiles("/root/repos/fans_club/mbTiles/0.mbtiles", async (err, mbtiles) => {
+new MBTiles("/root/repos/fans_club/mbTiles/0.mbtiles", (err, mbtiles) => {
   mbtiles.getInfo(function (err, info) {
     console.log(info); // info
   });
-  mbtiles.getTile(0, 0, 0, (err, tile) => {
+  mbtiles.getTile(0, 0, 0, async (err, tile) => {
     if (err) {
       console.log(err);
     } else {
       console.log(tile);
-      const {stdout} = await execa(`tippecanoe-decode ${tile} 0 0 0`)
+      const { stdout } = await execa(`tippecanoe-decode ${tile} 0 0 0`);
       // const buf = geobuf.decode(new P(tile));
       console.log(stdout);
     }
