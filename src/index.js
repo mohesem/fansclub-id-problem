49b0f6d554220962c53d4ed8981ddf5fb1a22581
mohesem <mohesem@gmail.com>
2020-08-18 12:18:55 +0000

    i dont care

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const { exec } = require("child_process");
       async (error, stdout, stderr) => {
         if (error) console.log(error);
         const obj = await JSON.parse(stdout);
-        console.log(obj.features);
+        console.log(obj.features[0].features.length);
         // console.log(Object.keys(obj));
         console.log(stderr);
       }
