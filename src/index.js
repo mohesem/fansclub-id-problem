14413bbf4aeb9b0e4fd1735c7aed46d611d9ea46
mohesem <mohesem@gmail.com>
2020-08-22 15:38:32 +0000

    i dont care

--- src/index.js
+++ src/index.js
@@ -16,61 +16,91 @@ const side = zoom * 2;
 let countX = 0;
 let countY = 0;
 
-const check = (z, x, y) => {
-  return new Promise((resolve, reject) => {
-    exec(
-      `tippecanoe-decode /root/repos/fans_club/mbTiles/0.mbtiles ${z} ${x} ${y}`,
-      { maxBuffer: 1024 * 10000 },
-      async (error, stdout, stderr) => {
-        if (error) console.log(error);
-        const obj = await JSON.parse(stdout);
-        console.log(obj.features[0].features.length);
-        obj.features[0].features.forEach(async (el, i) => {
-          // console.log(el);
-          const query = {
-            name0: el.properties.NAME_0,
-            name1: el.properties.NAME_1,
-            name2: el.properties.NAME_2,
-          };
-          console.log(query);
-          // if (el.NAME_0) query.name0 = el.NAME_0;
-          const res = await Geo.findOne(query).exec();
-          // console.log(res);
-          console.log(res.fid, el.id);
-          if (res.fid !== el.id) console.log("-----------------");
+// const check = (z, x, y) => {
+//   return new Promise((resolve, reject) => {
+//     exec(
+//       `tippecanoe-decode /root/repos/fans_club/mbTiles/1.mbtiles ${z} ${x} ${y}`,
+//       { maxBuffer: 1025 * 10000 },
+//       async (error, stdout, stderr) => {
+//         if (error) console.log(error);
+//         const obj = await JSON.parse(stdout);
+//         console.log(obj.features[1].features.length);
+//         obj.features[1].features.forEach(async (el, i) => {
+//           // console.log(el);
+//           const query = {
+//             name1: el.properties.NAME_0,
+//             name2: el.properties.NAME_1,
+//             name3: el.properties.NAME_2,
+//           };
+//           console.log(query);
+//           // if (el.NAME_1) query.name0 = el.NAME_0;
+//           const res = await Geo.findOne(query).exec();
+//           // console.log(res);
+//           console.log(res.fid, el.id);
+//           if (res.fid !== el.id) console.log("-----------------");
 
-          if (i === obj.features[0].features.length - 1) {
-            // console.log("done");
-            resolve();
-          }
-        });
-        console.log(stderr);
-      }
-    );
-  });
-};
+//           if (i === obj.features[1].features.length - 1) {
+//             // console.log("done");
+//             resolve();
+//           }
+//         });
+//         console.log(stderr);
+//       }
+//     );
+//   });
+// };
 
-console.log("start");
-mongoose.connection.on("connected", async () => {
-  console.log("connected to database");
-  try {
-    (async function loop() {
-      if (countY === side - 1 && countX === side - 1) {
-        console.log("done");
-        return false;
+// console.log("start");
+// mongoose.connection.on("connected", async () => {
+//   console.log("connected to database");
+//   try {
+//     (async function loop() {
+//       if (countY === side - 1 && countX === side - 1) {
+//         console.log("done");
+//         return false;
+//       }
+//       await check(zoom, countX, countY);
+//       if (countY < side) {
+//         countY += 1;
+//       }
+//       if (countY === side) {
+//         countY = 0;
+//         countX += 1;
+//       }
+//       // await check();
+//       loop();
+//     })();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+exec(
+  `tippecanoe-decode /root/repos/fans_club/mbTiles/0.mbtiles 5 20 12`,
+  { maxBuffer: 1024 * 10000 },
+  async (error, stdout, stderr) => {
+    if (error) console.log(error);
+    const obj = await JSON.parse(stdout);
+    console.log(obj.features[0].features.length);
+    obj.features[0].features.forEach(async (el, i) => {
+      // console.log(el);
+      const query = {
+        name0: el.properties.NAME_0,
+        name1: el.properties.NAME_1,
+        name2: el.properties.NAME_2,
+      };
+      console.log(query);
+      // if (el.NAME_0) query.name0 = el.NAME_0;
+      const res = await Geo.findOne(query).exec();
+      // console.log(res);
+      console.log(res.fid, el.id);
+      if (res.fid !== el.id) console.log("-----------------");
+
+      if (i === obj.features[0].features.length - 1) {
+        // console.log("done");
+        resolve();
       }
-      await check(zoom, countX, countY);
-      if (countY < side) {
-        countY += 1;
-      }
-      if (countY === side) {
-        countY = 0;
-        countX += 1;
-      }
-      // await check();
-      loop();
-    })();
-  } catch (error) {
-    console.log(error);
+    });
+    console.log(stderr);
   }
-});
+);
